8c30067bc8caccefe4fc8c877d7288bee649ddcd
xavierpenya <xavierpenya@gmail.com>
2017-06-06 11:48:44 +0200

    Added all raw data from the website.

--- src/Lausanne20Km.Tests/ParseTests.cs
+++ src/Lausanne20Km.Tests/ParseTests.cs
@@ -9,8 +9,15 @@ namespace Lausanne20Km.Tests
     [TestClass]
     public class ParseTests
     {
-        private const string WebResponseTestDataFile = @"..\..\..\..\..\data\web-response-2017-A.htm";
-        private const string CsvDataFile  = @"..\..\..\..\..\data\data.txt";
+        private const string BasePath = @"..\..\..\..\..\data\";
+        
+        private const string CsvDataFile  = BasePath + "data.txt";
+        private const string RawWebDataDirectory = BasePath + "raw-web-data";
+
+        public ParseTests()
+        {
+            Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+        }
 
         [TestMethod]
         public void DataFileExists_IOFile_Exists()
@@ -35,7 +42,8 @@ namespace Lausanne20Km.Tests
         [TestMethod]
         public void HtmlContainsData_LocalHtmlFile_AnyResults()
         {
-            var webpageResponseStr = File.ReadAllText(WebResponseTestDataFile);
+            var filePath = GetTestWebResponseDataFile();
+            var webpageResponseStr = File.ReadAllText(filePath);
             var results = RaceResultWebRepository.GetDataLinesFromWebResponse(webpageResponseStr);
             Assert.IsTrue(results.Any());
         }
@@ -43,10 +51,37 @@ namespace Lausanne20Km.Tests
         [TestMethod]
         public void HtmlIsParsed_LocalHtmlFile_AnyResults()
         {
-            var webpageResponseStr = File.ReadAllText(WebResponseTestDataFile);
+            var filePath = GetTestWebResponseDataFile();
+            var webpageResponseStr = File.ReadAllText(filePath);
             var dataLines = RaceResultWebRepository.GetDataLinesFromWebResponse(webpageResponseStr);
             var parsedResults = RaceResultWebRepository.ParseLines(2017, dataLines);
             Assert.IsTrue(dataLines.Any());
+        }        
+
+#region "Helpers"
+
+        private static string GetTestWebResponseDataFile()
+            => Path.Combine(RawWebDataDirectory, "2017", $"A.html");
+
+        [Ignore]
+        [TestMethod]
+        public void DownloadAllResultPages()
+        {
+            for (var year = 2017; year >= 2009; year--)
+            {
+                Console.WriteLine(year);
+                Directory.CreateDirectory(Path.Combine(RawWebDataDirectory, year.ToString()));
+                for (var letter = 'A'; letter <= 'Z'; letter++)
+                {
+                    Console.WriteLine(letter);
+                    var webpageResponseStr = RaceResultWebRepository.DownloadWebpageStr(year, letter);
+                    var filePath = Path.Combine(RawWebDataDirectory, year.ToString(), $"{letter}.html");
+                    File.WriteAllText(filePath, webpageResponseStr);
+                }
+            }
         }
+
+#endregion
+
     }
 }
