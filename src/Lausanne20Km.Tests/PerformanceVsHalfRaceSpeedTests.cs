186fdb7f698c0d198f16c6dfd3b8c72836435362
xavierpenya <xavierpenya@gmail.com>
2017-06-06 17:19:02 +0200

    Modified tests.

--- src/Lausanne20Km.Tests/PerformanceVsHalfRaceSpeedTests.cs
+++ src/Lausanne20Km.Tests/PerformanceVsHalfRaceSpeedTests.cs
@@ -21,20 +21,36 @@ namespace Lausanne20Km.Tests
             Directory.SetCurrentDirectory(AppContext.BaseDirectory);
         }
         
-        [Ignore]
+        //[Ignore]
         [TestMethod]
         public void CsvIsLoaded_CsvFile_AnyResults()
         {
             var raceResults = RaceResultCsvRepository.GetAll(CsvDataFile);
 
-            var xyResultsMen = PerformanceVsHalfRaceSpeedAnalyzer.GetXYResults(raceResults, distance: 20, minDataSize: 100, gender: Gender.Male);
+            var xyResultsMen = PerformanceVsHalfRaceSpeedAnalyzer.GetXYResults(raceResults, distance: 20, minDataSize: 200, gender: Gender.Male);
+            var xyResultsWomen = PerformanceVsHalfRaceSpeedAnalyzer.GetXYResults(raceResults, distance: 20, minDataSize: 200, gender: Gender.Female);
 
-            var str = PerformanceVsHalfRaceSpeedAnalyzer.ParseToXYColumnStr(xyResultsMen.Values.ToList());
+            var menResults2017 = xyResultsMen
+                .Where(x => x.Key.year == "2017" && x.Value.AgeComparativePerformanceRatio < 1)
+                .ToDictionary(x => x.Key, x => x.Value );
+            var womenResults2017 = xyResultsWomen
+                .Where(x => x.Key.year == "2017" && x.Value.AgeComparativePerformanceRatio < 1)
+                .ToDictionary(x => x.Key, x => x.Value);
 
-            var selectedQuadrantResults = xyResultsMen
-                .Where(x => x.Value.AgeComparativePerformanceRatio < 0.7 && x.Value.FirstHalfVsTotalTimeSpeedRatio > 0.55)
-                .Select(x => GetResultAsStr(x.Key))
+            var str1 = PerformanceVsHalfRaceSpeedAnalyzer.ParseToXYColumnStr(menResults2017.Values.ToList());
+            var str2 = PerformanceVsHalfRaceSpeedAnalyzer.ParseToXYColumnStr(menResults2017.Values.ToList());
+
+            var str = str1 + str2;
+
+            var selectedQuadrantResults = menResults2017
+                .Where(x => x.Value.AgeComparativePerformanceRatio < 0.65)
+                .Select(x => x.Key)
                 .ToList();
+
+            //var selectedQuadrantResults = xyResultsMen
+            //    .Where(x => x.Value.AgeComparativePerformanceRatio < 0.7 && x.Value.FirstHalfVsTotalTimeSpeedRatio > 0.55)
+            //    .Select(x => GetResultAsStr(x.Key))
+            //    .ToList();
         }
 
         private static string GetResultAsStr(RaceResult raceResult)
